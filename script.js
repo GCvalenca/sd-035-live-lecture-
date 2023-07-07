7d31d97ef90087f885d89b31263d535fb295c4d0
GCValenca <guguethh@gmail.com>
2023-07-07 18:45:51 -0300

    resolve o acerto da mega sena

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-let lotteryNumbers = [4, 8, 15, 16, 23, 42];
+let lotteryNumbers = [];
+let firstGame = [7, 14, 35, 48, 51, 60];
 
-for (let index = 0; index < lotteryNumbers.length; index += 1) {
-    console.log(`O index ${index} possui o valor ${lotteryNumbers[index]}`);
+// Gera os numeros da mega-sena
+for (let index = 0; index < firstGame.length; index += 1) {
+   lotteryNumbers.push(Math.ceil(Math.random() * 60));
+};
+
+// Comparação do sorteio com o jogo do vito
+let count = 0;
+let hits = [];
+
+for (let indexLottery = 0; indexLottery < lotteryNumbers.length; indexLottery += 1) {  
+    for (let indexGame = 0; indexGame < firstGame.length; indexGame += 1) {
+        if (lotteryNumbers[indexLottery] === firstGame[indexGame]) {
+            count += 1;
+            hits.push(lotteryNumbers[indexLottery]);
+        }
+    }  
 }
+
+// QuantidlotteryNumberse acertos
+console.log('mega sena:', lotteryNumbers);
+console.log('jogo', firstGame);
+console.log('acertos', count);
+console.log("quais", hits);
