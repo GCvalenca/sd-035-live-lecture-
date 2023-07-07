77df63bd5fd3b839911c24196c998e59766f036d
GCValenca <guguethh@gmail.com>
2023-07-07 16:38:16 -0300

    começando aula 3.2

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let num1 = 10;
-let num2 = 10;
 
-if (num1 > num2) {
-    console.log('num 1 wins');
-} else if (num1 === num2) {
-    console.log ('Deu empate!')
-} else {
-    console.log ('num2 wins')
+
+let diaper;
+const babySize = 5;
+
+if (babySize <= 4) {
+    diaper = "RN";
+} else if (babySize > 4 && babySize <= 6) {
+    diaper = "RN+"
+
 }
-
-console.log(1 + 1)
+console.log('o tamanho é' + diaper);
